75dedb7d54c04c043e5c31117738a6b31a9066d5
rfarmaha <ramandeep.f@gmail.com>
2017-01-06 01:02:44 -0500

    Changed some set up to get heroku deploys working

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 from app import views
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('summarizr startup')
--- run.py
+++ run.py
@@ -2,5 +2,4 @@
 from app import app
 import config
 if __name__ == '__main__':
-    app.config.from_object(config.DevelopmentConfig)
     app.run()
